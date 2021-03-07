4c7b5ab4ad28b91226e7a8ecc983867e913e494c
Tusk <80130306+Tusk-Drive@users.noreply.github.com>
2021-03-06 22:02:13 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATE_TIME_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("DATE-TIME Calculator");
+            Console.WriteLine(String.Format("Short date: {0:d}", today));
+            Console.WriteLine(String.Format("Long date: {0:D}", today));
+            Console.WriteLine(String.Format("Short time: {0:t}", today));
+            Console.WriteLine(String.Format("Long time: {0:T}", today));
+            Console.WriteLine(String.Format("Month: {0:M}", today));
+            Console.WriteLine(String.Format("Year: {0:Y}", today));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
